d8ec03c0c3e931db9840ff596bc605c466e36bf6
James Stewart <jsstewar@qualcomm.com>
2014-03-28 16:15:38 -0500

    set execution flow and move all constants to this file

--- scripts/decrypt_and_move.rb
+++ scripts/decrypt_and_move.rb
@@ -3,7 +3,17 @@
 require_relative "../lib/decrypt"
 require_relative "../lib/copy_file"
 
-puts "It should decrypt a file"
-puts "It should store decrypted file in 'files' directory"
-puts "It does copy from 'files' directory to another directory"
-CopyFile.cpfiles
+# Decrypt variables
+  PGPFILE = "<dir where PGP file lives"
+  KEYFILE = "keys/<key file>"
+# CopyFile variables
+  DEST = "<destination dir for decrypted file"
+
+puts "File decrypted"
+decrypted = Decrypt.decrypt_file
+
+puts "Decrypted file stored"
+File.open("files/out.txt", 'w') {|f| f.write(decrypted) }
+
+puts "Decrypted file copied"
+CopyFile.mvfiles
