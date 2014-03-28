09d1286f5834494b4eb4621f68bc8f313456d21a
James Stewart <jsstewar@qualcomm.com>
2014-03-28 16:14:25 -0500

    decrypt pgp file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,8 +1,26 @@
 #!/usr/bin/env ruby
 
-require "gpgme"
-
 module Decrypt
+  require "gpgme"
   extend self
+
+  def passfunc(obj, uid_hint, passphrase_info, prev_was_bad, fd)
+    io = IO.for_fd(fd, 'w')
+    io.puts "PASSPHRASE"
+    io.flush
+  end
+
+  def decrypt_file
+    encrypted_data = GPGME::Data.new(File.open("#{PGPFILE}"))
+    key = GPGME::Data.new(File.open("#{KEYFILE}"))
+
+    ctx = GPGME::Ctx.new :passphrase_callback => method(:passfunc)
+    ctx.import_keys key
+
+    decrypted = ctx.decrypt encrypted_data
+    decrypted.seek(0)
+
+    return decrypted.read
+  end
 
 end
